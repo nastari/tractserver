26fa69a48b9727b0c674727c1386c1ff3fec5b4f
Gabriel <gabrielnabas@id.uff.br>
2020-11-17 02:14:17 -0300

    .

--- src/validations/asset.js
+++ src/validations/asset.js
@@ -3,9 +3,8 @@ import * as Yup from 'yup';
 export const store = (data) => {
   const schema = Yup.object().shape({
     name: Yup.string().required(),
-    // startup: Yup.date().required(),
     description: Yup.string().required(),
-    userOwner: Yup.string().required(),
+    owner_id: Yup.string().required(),
     status: Yup.string().required(),
   });
 
